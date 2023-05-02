733cc9051f4283ce5ea9ca4b46ba7d5ba6739ca2
Aleksandr-Alekseenko-23 <alekseenko48@bigmir.net>
2023-05-02 19:14:01 +0300

    fix router

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -14,8 +14,8 @@ const AppRoutes: React.FC = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="tweets" element={<TweetsPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
-        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Suspense>
   );
